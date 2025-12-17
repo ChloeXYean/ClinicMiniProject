84b2fcd5699ff38d40140d241114247a4688b705
Chloe Ng <yeanchloe@gmail.com>
2025-12-17 16:23:40 +0800

    Update for nurse nav

--- ClinicMiniProject/AppDbContext.cs
+++ ClinicMiniProject/AppDbContext.cs
@@ -16,59 +16,24 @@ namespace ClinicMiniProject
             //Database.EnsureCreated();
         }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        //string server = Environment.GetEnvironmentVariable("DB_SERVER_IP") ?? "172.16.59.30";
-        //        //string server = Environment.GetEnvironmentVariable("DB_SERVER_IP") ?? "localhost";
-        //        //string connStr = $"Server={server};Port=3306;Database=testdb;Uid=root;Pwd=123456;Charset=utf8mb4;";
-        //        string connStr = $"Server=localhost;Port=3306;Database=testdb;Uid=root;Pwd=123456;Charset=utf8mb4;";
-
-        //        optionsBuilder.UseMySql(
-        //            connStr,
-        //            ServerVersion.AutoDetect(connStr),
-        //            options => options.EnableRetryOnFailure()
-        //        );
-        //    }
-        //}
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        var connStr =
-        //            "Server=127.0.0.1;" +
-        //            "Port=3306;" +
-        //            "Database=testdb;" +
-        //            "Uid=root;" +
-        //            "Pwd=123456;" +
-        //            "SslMode=None;" +
-        //            "Charset=utf8mb4;";
-
-        //        optionsBuilder.UseMySql(
-        //            connStr,
-        //            new MySqlServerVersion(new Version(9, 0, 0)),
-        //            options => options.EnableRetryOnFailure()
-        //        );
-        //    }
-        //}
-
-        // ClinicMiniProject/AppDbContext.cs
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                // Default to localhost for Windows
-                string serverIp = "127.0.0.1";
 
-                // If running on Android, change IP to 10.0.2.2 (which points to your PC)
-#if ANDROID
-                serverIp = "10.0.2.2";
-#endif
+                //string serverIp = "ballast.proxy.rlwy.net";
+                //var connStr =
+                //    $"Server={serverIp};" +
+                //    "Port=19463;" +
+                //    "Database=testdb;" +
+                //    "Uid=root;" +
+                //    "Pwd=NrIvCewJcTGAPqmOXyoziksWgwoQmaQd;" +
+                //    "SslMode=Required;" +
+                //    "Charset=utf8mb4;";
 
+                string serverIp = "localhost";
                 var connStr =
-                    $"Server={serverIp};" + // Use the variable here
+                    $"Server={serverIp};" +
                     "Port=3306;" +
                     "Database=testdb;" +
                     "Uid=root;" +
@@ -81,6 +46,7 @@ namespace ClinicMiniProject
                     new MySqlServerVersion(new Version(9, 0, 0)),
                     options => options.EnableRetryOnFailure()
                 );
+
             }
         }
 
@@ -89,8 +55,8 @@ namespace ClinicMiniProject
         {
             modelBuilder.Entity<Staff>()
                 .HasOne(s => s.Availability)
-                //.WithOne(d => d.Staff)
-                .WithOne()
+                .WithOne(d => d.Staff)
+                //.WithOne()
                 .HasForeignKey<DocAvailable>(d => d.staff_ID);
 
             // Ensure one-to-one relationship between Staff and DocAvailable
