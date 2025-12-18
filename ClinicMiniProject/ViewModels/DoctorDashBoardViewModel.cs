9c6288757c2bff7b24cd202b3b870e8e635746e6
winwin <jhieying@gmail.com>
2025-12-18 18:59:24 +0800

    got prb

--- ClinicMiniProject/ViewModels/DoctorDashBoardViewModel.cs
+++ ClinicMiniProject/ViewModels/DoctorDashBoardViewModel.cs
@@ -59,6 +59,7 @@ namespace ClinicMiniProject.ViewModels
         public ICommand NavigateToHomeCommand { get; }
         public ICommand NavigateToInquiryCommand { get; }
         public ICommand NavigateToProfileCommand { get; }
+        public ICommand NavigateToInquiryHistoryCommand { get; }
 
         public ICommand ToggleMenuCommand { get; }
         public ICommand NotificationCommand { get; }
@@ -77,8 +78,10 @@ namespace ClinicMiniProject.ViewModels
             NavigateToReportingManagementCommand = new Command(async () => await Shell.Current.GoToAsync("ReportingManagement"));
 
             // Bottom Bar Commands
-            NavigateToInquiryCommand = new Command(async () => await Shell.Current.GoToAsync("Inquiry"));
-            NavigateToProfileCommand = new Command(async () => await Shell.Current.GoToAsync("Profile"));
+            NavigateToInquiryCommand = new Command(async () => await Shell.Current.GoToAsync("InquiryHistory"));
+            NavigateToInquiryHistoryCommand = new Command(async () => await Shell.Current.GoToAsync("InquiryHistory"));
+            NavigateToProfileCommand = new Command(async () => await Shell.Current.GoToAsync("DoctorProfile"));
+            NavigateToHomeCommand = new Command(async () => await Shell.Current.GoToAsync("DoctorDashboardPage"));
 
             LogoutCommand = new Command(async () =>
             {
